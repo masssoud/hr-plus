a88e2986378528e286ab684e210eea16c2ef6823
Hirad Daneshvar <hirad.daneshvar@gmail.com>
2020-02-23 17:33:39 +0330

    Job Model changed

--- jobs/models/job.py
+++ jobs/models/job.py
@@ -3,7 +3,12 @@ from django.db import models
 
 class Job(models.Model):
     title = models.CharField(max_length=200)
+    description = models.TextField()
+    requirements = models.TextField()
+    good_to_have = models.TextField()
+    is_open = models.BooleanField(default=True, db_index=True)
     pub_date = models.DateTimeField('date published')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL)
 
     class Meta:
         db_table = 'jobs'

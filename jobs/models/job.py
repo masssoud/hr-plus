79c09242afe8c47c84b11cdefe11e57b31b0e091
Hirad Daneshvar <hirad.daneshvar@gmail.com>
2020-02-23 17:49:33 +0330

    Job and Category added to Admin

--- jobs/models/job.py
+++ jobs/models/job.py
@@ -9,6 +9,8 @@ class Job(models.Model):
     is_open = models.BooleanField(default=True, db_index=True)
     pub_date = models.DateTimeField('date published')
     category = models.ForeignKey('Category', on_delete=models.SET_NULL)
+    created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         db_table = 'jobs'
